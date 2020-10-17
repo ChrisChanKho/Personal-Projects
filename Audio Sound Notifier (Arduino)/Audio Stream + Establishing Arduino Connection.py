90e76b1f426a30f78cf3e40e97ecde84deba0051
ChrisChanKho <46541246+ChrisChanKho@users.noreply.github.com>
2020-10-17 11:55:45 +0100

    Update and rename Waveform Visual + Establishing Arduino Connection.py to Audio Stream + Establishing Arduino Connection.py

--- Audio Sound Notifier (Arduino)/Audio Stream + Establishing Arduino Connection.py
+++ Audio Sound Notifier (Arduino)/Audio Stream + Establishing Arduino Connection.py
@@ -24,9 +24,6 @@ while True:
         print("Arduino Not Found ..." + "\n")
         time.sleep(5)
 
-# create matplotlib figure and axes
-fig, ax = plt.subplots(1, figsize=(15, 7))
-
 # pyaudio class instance
 p = pyaudio.PyAudio()
 
@@ -40,27 +37,9 @@ stream = p.open(
     frames_per_buffer=CHUNK
 )
 
-# variable for plotting
-x = np.arange(0, 2 * CHUNK, 2)
-
-# create a line object with random data
-line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=1)
-
-# basic formatting for the axes
-ax.set_title('AUDIO WAVEFORM')
-ax.set_xlabel('samples')
-ax.set_ylabel('amplitude')
-ax.set_ylim(0, 255)
-ax.set_xlim(0, 2 * CHUNK)
-plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[-255, 0, 255])
-
-# show the plot
-plt.show(block=False)
-
-# for measuring frame rate
+# for measuring volume
 sampleCount = 0
 volCum = 0
-avg = 0
 start_time = time.time()
 
 while True:
@@ -74,12 +53,8 @@ while True:
     # create np array
     data_np = np.array(data_int, dtype='b')[::2]
 
-    line.set_ydata(data_np)
-
-    # update figure canvas
+    # Updating variable trackers per sample
     try:
-        fig.canvas.draw()
-        fig.canvas.flush_events()
         sampleCount += 1
         volCum += data_np[1]
 
@@ -89,14 +64,11 @@ while True:
             volCum = 0
             sampleCount = 0
 
-    # When user clicks the exit window of the graphic
-    except TclError:
-
-        print('stream stopped')
-        break
-
     # Error handling for lost Arduino connection
     except (serial.serialutil.SerialException, FileNotFoundError):
 
         print("Connection Lost ... Program has ended")
         break
+
+
+
